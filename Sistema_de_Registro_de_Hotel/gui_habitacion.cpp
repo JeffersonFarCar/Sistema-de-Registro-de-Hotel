1c8e9553131213210bd3aba6327fed561c53ceea
edilsonw <52023262+edilsonw@users.noreply.github.com>
2020-07-02 02:37:07 -0500

    vista habitacion

--- Sistema_de_Registro_de_Hotel/gui_habitacion.cpp
+++ Sistema_de_Registro_de_Hotel/gui_habitacion.cpp
@@ -3,6 +3,12 @@
 #include "gui_tipo_habitacion.h"
 #include "mainwindow.h"
 #include "gui_estado_habitacion.h"
+
+#include <QMessageBox>
+#include <iostream>
+#include <string>
+using namespace std;
+
 gui_habitacion::gui_habitacion(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::gui_habitacion)
@@ -39,4 +45,24 @@ void gui_habitacion::on_pushButton_2_clicked()
     gui_estado_habitacion guiEH;
     guiEH.setModal(true);
     guiEH.exec();
+}
+
+void gui_habitacion::on_pushButton_3_clicked()
+{
+    QString idH_str = ui->lineEdit_2->text();
+    QString numH_str = ui->lineEdit_3->text();
+    QString numP_str = ui->lineEdit_4->text();
+
+    try {
+        int idH = stoi(idH_str.toLocal8Bit().data());
+        int numH = stoi(numH_str.toLocal8Bit().data());
+        int numP = stoi(numP_str.toLocal8Bit().data());
+    } catch (invalid_argument const &e) {
+        QMessageBox::warning(this, "Advertencia", "Ingreso de datos erroneo.");
+    }
+}
+
+void gui_habitacion::on_pushButton_4_clicked()
+{
+    close();//Permite cerra la ventana actual abierta
 }
