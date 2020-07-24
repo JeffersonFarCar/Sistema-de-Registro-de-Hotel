d900529b9efec455f70be26c8acf100184a6d03e
JeffersonFarCar <jfarfanc@unsa.edu.pe>
2020-07-24 11:02:23 -0500

    Base de datos modificada

--- Sistema_de_Registro_de_Hotel/conexion.cpp
+++ Sistema_de_Registro_de_Hotel/conexion.cpp
@@ -22,21 +22,6 @@ void  Conexion::Cerrar(){
     db.close();
 }
 
-void Conexion::addEmpleado(Empleado e){
-    QSqlQuery query;
-    QString nombre=QString::fromLocal8Bit(e.getNombre().c_str());
-    QString apellido=QString::fromLocal8Bit(e.getApellido().c_str());
-    QString direccion=QString::fromLocal8Bit(e.getDireccion().c_str());
-    QString email=QString::fromLocal8Bit(e.getEmail().c_str());
-    QString ocupacion=QString::fromLocal8Bit(e.getOcupacion().c_str());
-    QString fechaContratacion=QString::fromLocal8Bit(e.getFechaContratacion().c_str());
-    QString consulta="insert into empleados (id, nombre, apellido, direccion, email, ocupacion, sueldo, fechaContratacion) values ("+QString::number(e.getId())+",'"+nombre+"','"+apellido+"','"+direccion+"','"+email+"','"+ocupacion+"',"+QString::number(e.getSueldo())+",'"+fechaContratacion+"');";
-    cout<<consulta.toUtf8().constData();
-    query.exec(consulta);
-    query.isActive();
-
-}
-
 void Conexion::addHabitacion(Habitacion a){
     QSqlQuery query;
      // int id =a.getIdHabitacion();
